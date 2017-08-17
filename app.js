26da1925acb70ba5bb36d5ee09ca505e591b65e2
Tyler Rhinehart <rhinehart22@gmail.com>
2017-08-16 20:13:05 -0600

    changed app

--- app.js
+++ app.js
@@ -5,3 +5,8 @@
 //         'slow');
 // })
 
+
+//On scroll of page, draw new part of greater page i.e. default the header, onscroll draw skills, then portfolio, etc
+function onScroll() {
+    draw
+}
